0f9d783f55d43849c62147abf3697dc4f9a2cc2d
duxiaolei <duxiaolei@mabangerp.com>
2022-01-18 21:14:08 +0800

    add hooks

--- demo-serve/build/build.js
+++ demo-serve/build/build.js
@@ -1,11 +1,37 @@
 const Webpack = require('webpack');
+const ora = require('ora');
+const chalk = require('chalk');
 const webpackConfig = require('./webpack.config.prod');
-
 const compiler = Webpack(webpackConfig);
 
+const spinner = ora('building for production...\n\n');
+spinner.start();
+
 compiler.run((err, stats) => {
-  console.log('run');
-  compiler.close((err, result) => {
-    console.log('close');
+  if (err) {
+    throw err;
+  }
+
+  process.stdout.write(
+    stats.toString({
+      colors: true,
+      modules: false,
+      children: true,
+      chunks: true,
+      chunkModules: false,
+    }) + '\n\n'
+  );
+
+  if (stats.hasErrors()) {
+    console.log(chalk.red('  Build failed with errors.\n'));
+    process.exit();
+  }
+  compiler.close((err) => {
+    spinner.stop();
+    if (err) {
+      throw err;
+    }
+    console.log(chalk.bold.greenBright('  Build complete.\n'));
+    console.log(chalk.green('  Tip: built files are meant to be served over an HTTP server.\n' + "  Opening index.html over file:// won't work.\n"));
   });
 });
--- demo-serve/build/server.js
+++ demo-serve/build/server.js
@@ -1,14 +1,21 @@
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack.config.dev.js');
+const ora = require('ora');
 
 const compiler = Webpack(webpackConfig);
 const devServerOptions = { ...webpackConfig.devServer, open: false };
 const server = new WebpackDevServer(devServerOptions, compiler);
 
+const spinner = ora('starting server....\n');
+
 async function runServer() {
-  console.log('==== starting server. ====');
   await server.start();
+  spinner.start();
+
+  compiler.hooks.done.tap('html-webpack-plugin', () => {
+    spinner.stop();
+  });
 }
 
 runServer();
